d8e2b25c40b649b1d3213c20ebdadf16ff781c4f
siriwat191260 <pan-191260@hotmail.com>
2021-01-19 16:56:02 +0700

    finish

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,26 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N,double B[])
+{
+    double sum = 0,min = A[0],max = A[0],sum_std = 0,multiply = 1,sum_har;
+    for(int i = 0; i < N;i++)
+    {
+        sum += A[i];
+        multiply *= A[i];
+        sum_har += 1.0/A[i];
+        if(A[i] > max) max = A[i];
+        if(A[i] < min) min = A[i];
+    }
+    B[0]=sum/N;
+    for(int i = 0; i < N;i++)
+    {
+        sum_std += (pow(A[i],2)-pow(B[0],2));
+    }
+    B[1]=sqrt(1.0/N*sum_std);
+    B[2] = pow(multiply,1.0/N);
+    B[3] = N / sum_har;
+    B[4] = max;
+    B[5] = min;
+}   
